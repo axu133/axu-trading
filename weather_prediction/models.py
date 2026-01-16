26007b0a5f23ea3a5197e1efee141611b74eaa82
Andrew Xu <yandrew.xu@gmail.com>
2026-01-15 23:59:43 -0500

    3D CNN implemented, training script.

--- weather_prediction/models.py
+++ weather_prediction/models.py
@@ -1,7 +1,58 @@
 import torch
-from torch.nn import nn
+import torch.nn as nn
+import numpy as np
 
 class Weather3DCNN(nn.Module):
     def __init__(self, input_channels = 5, input_frames = 20):
         super().__init__()
+        self.conv1 = nn.Conv3d(in_channels=input_channels, out_channels=32, kernel_size=(3, 3, 3), padding=1, stride = (1, 2, 2))
+        self.bn1 = nn.BatchNorm3d(32)
+        self.relu = nn.ReLU()
+
+        self.conv2 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=(3, 3, 3), padding=1, stride = (2, 2, 2))
+        self.bn2 = nn.BatchNorm3d(64)
+
+        self.conv3 = nn.Conv3d(in_channels=64, out_channels=128, kernel_size=(3, 3, 3), padding=1, stride = (2, 2, 2))
+        self.bn3 = nn.BatchNorm3d(128)
+
+        self.pool = nn.AdaptiveAvgPool3d((1, 1, 1))
+
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(64, 1)
+        )
+    
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.relu(self.bn3(self.conv3(x)))
+
+        x = self.pool(x)
+        x = self.fc(x)
+        return x
+    
+class EarlyStopping:
+    def __init__(self, patience=10, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.best_loss = np.inf
+        self.early_stop = False
+
+    def __call__(self, val_loss):
+        if val_loss < (self.best_loss - self.min_delta):
+            self.best_loss = val_loss
+            self.counter = 0
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+                
+        return self.early_stop
         
+if __name__ == "__main__":
+    device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+    model = Weather3DCNN(input_channels=5, input_frames=20).to(device)
--- weather_prediction/train_daily_max_min.py
+++ weather_prediction/train_daily_max_min.py
@@ -2,12 +2,87 @@ import torch
 import torch.nn as nn
 import numpy as np
 from read_data import ERA5Dataset
+from models import Weather3DCNN, EarlyStopping
+from timeit import default_timer
+import copy
+from datetime import date
+import os
 
+seed_num = 1
+torch.manual_seed(seed_num)
+torch.mps.manual_seed_all(seed_num)
 device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+model_directory = 'models/'
+date_str = date.today().strftime("%Y%m%d")
+min_or_max = 'max'
 
 if __name__ == "__main__":
-    dataset = ERA5Dataset(years=range(2015, 2016), window_size=5, max_or_min='max')
-    print(f"Dataset size: {len(dataset)}")
-    sample_data, sample_target = dataset[0]
-    print(f"Sample data shape: {sample_data.shape}")
-    print(f"Sample target: {sample_target}")
+    dataset = ERA5Dataset(years=range(2015, 2016), window_size=5, max_or_min=min_or_max)
+
+    model = Weather3DCNN(input_channels=5, input_frames=20).to(device)
+    stopper = EarlyStopping(patience=10, min_delta=0.0001)
+
+    total_samples = len(dataset)
+    train_size = int(0.8 * total_samples)
+    test_size = total_samples - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+    batch_size = 64
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+
+    lr = 0.001
+    num_epochs = 500
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3, factor=0.5)
+    loss_fn = nn.MSELoss()
+
+    train_losses = []
+    test_losses = []
+    min_test_loss = np.inf
+
+    for i in range(num_epochs):
+        model.train()
+        t1 = default_timer()
+        total_train_loss = 0.0
+        for batch_data, batch_target in train_dataset:
+            batch_data, batch_target = batch_data.to(device), batch_target.to(device)
+            
+            y = model(batch_data)
+            loss = loss_fn(y, batch_target)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total_train_loss += loss.item() * batch_data.size(0)
+        
+        model.eval()
+        with torch.no_grad():
+            for batch_data, batch_target in test_dataset:
+                total_test_loss = 0.0
+                batch_data, batch_target = batch_data.to(device), batch_target.to(device)
+
+                preds = model(batch_data)
+                loss = loss_fn(preds, batch_target)
+                total_test_loss += loss.item() * batch_data.size(0)
+            
+        avg_train_loss = total_train_loss / train_size
+        avg_test_loss = total_test_loss / test_size
+
+        train_losses.append(avg_train_loss)
+        test_losses.append(avg_test_loss)
+
+        if avg_test_loss < min_test_loss:
+            min_test_loss = avg_test_loss
+            best_model_state = copy.deepcopy(model)
+        
+        scheduler.step(avg_test_loss)
+        print(f"Epoch: {i + 1}, Avg Train Loss: {avg_train_loss}, Avg Test Loss: {avg_test_loss}, LR: {scheduler.get_last_lr()}, Time taken: {default_timer()-t1}")
+
+        if stopper(avg_test_loss):
+            print("Early stopping triggered.")
+            break
+
+    model_path = os.path.join(model_directory, f'daily_max_model_{date_str}_{min_or_max}.pth')
+    torch.save(best_model_state.state_dict(), model_path)
+            
